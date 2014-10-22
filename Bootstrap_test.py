e857e756a07f316e976735cdcf07bd0836374416
Helio Mendonca <hsmptg@gmail.com>
2014-10-22 17:38:22 +0100

    bootstrap-switch

--- Bootstrap_test.py
+++ Bootstrap_test.py
@@ -10,20 +10,16 @@ def main():
     def index():
         return render_template('index.html')
 
+    @app.route('/2')
+    def index2():
+        return render_template('index2.html')
+
+    @app.route('/3')
+    def index3():
+        return render_template('index3.html')
+
     app.run(host='0.0.0.0', port=5001)
-    
-def create_app():
-    app = Flask(__name__)
-    app.debug = True
-    Bootstrap(app)
-
-    @app.route('/')
-    def index():
-        return render_template('index.html')
-
-    return app
 
 if __name__ == '__main__':
-#    create_app().run(host='0.0.0.0', port=5001)
     main()
     
